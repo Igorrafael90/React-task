5e20b07ad4aa938b1f60876616fb606a86b645c7
Igorrafael90 <rafaeligor804@gmail.com>
2024-09-12 15:42:14 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import gasolina from "./img/gasolina.jpg"
+import "./App.css"
 
-function App() {
+export default function App() {
+  const[number1, setnumber1] = useState(0);
+  const[number2, setnumber2] = useState(0);
+  const[result, setresult] = useState(0);
+  const[response, setresponse] = useState("");
+
+  function resultado() {
+    const calculo = (Number(number1) / Number(number2))
+    setresult(calculo);
+    if(calculo < 0.7){
+      setresponse("alcoll")
+    }else if(number1 <= 0 && number2 <= 0){
+      window.alert("Digite um numero valido")
+      setresult(0)
+      setresponse("digite numero valido")
+    }else{
+      setresponse("gasolina")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <img src={gasolina}></img>
+      <label>Insira o valor da gasolina</label>
+      <input
+        type="number"
+        value={number1}
+        onChange={e => setnumber1(e.target.value)}
+      />
+      <label>Insira o valor do alcoll</label>
+      <input
+        type="number"
+        value={number2}
+        onChange={e => setnumber2(e.target.value)}
+      />
+      <button onClick={resultado}>Somar</button>
+      <p>O preço foi de: {result.toFixed(2)}</p>
+      <p>mais vantajoso: {response}</p>
+    </div> 
   );
 }
-
-export default App;
